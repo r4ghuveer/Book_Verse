be3e1de0c64832d2d4ebac9ab2428e92dd6d0919
r4ghuveer <raghuveerofficial08@gmail.com>
2023-11-26 21:26:34 +0530

    csrf attack

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const path = require('path');
-
-
+const csrf = require('csurf');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+
+const csrfProtection = csrf();
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -37,6 +38,8 @@ app.use(
   })
 );
 
+app.use(csrfProtection);
+
 app.use((req, res, next) => {
   if (!req.session.user) {
     return next();
@@ -49,6 +52,11 @@ app.use((req, res, next) => {
     .catch(err => console.log(err));
 });
 
+app.use((req,res,next)=>{
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken;
+    next();
+})
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
@@ -58,18 +66,6 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Max',
-          email: 'max@test.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
       console.log("connected");
   })
