514c0798fe078eb72dc47b44ddfe6af032d7398c
r4ghuveer <raghuveerofficial08@gmail.com>
2023-12-31 23:07:46 +0530

    deleting images of products

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const fileHelper = require('../util/file');
 const {validationResult} = require('express-validator')
 
 exports.getAddProduct = (req, res, next) => {
@@ -140,6 +141,7 @@ exports.postEditProduct = (req, res, next) => {
             product.price = updatedPrice;
             product.description = updatedDesc;
             if(image){
+                fileHelper.deleteFile(product.imageUrl);
                 product.imageUrl = image.path;
             }
             return product.save().then(result => {
@@ -176,8 +178,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.deleteOne({_id: prodId, userId: req.user._id})
-        .then(() => {
+    Product.findById(prodId).then(product => {
+        if(!product){
+            return next(new Error('Product not found'));
+        }
+        fileHelper.deleteFile(product.imageUrl);
+    return Product.deleteOne({_id: prodId, userId: req.user._id})
+    }).then(() => {
             console.log('DESTROYED PRODUCT');
             res.redirect('/admin/products');
         })
