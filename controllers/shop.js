7d09bd8679cac3f534267d543799097575a5d3ac
r4ghuveer <raghuveerofficial08@gmail.com>
2024-01-05 03:29:15 +0530

    Added Pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,19 +1,32 @@
 const Product = require('../models/product');
 const PDFDocument = require('pdfkit');
 const Order = require('../models/order');
+const ITEMS_PER_PAGE = 1;
 const fs=require('fs');
 const path = require('path')
 exports.getProducts = (req, res, next) => {
-    Product.find()
-        .then(products => {
-            console.log(products);
-            res.render('shop/product-list', {
-                prods: products,
-                pageTitle: 'All Products',
-                path: '/products',
-                isAuthenticated: req.session.isLoggedIn
-            });
-        })
+    const page = +req.query.page || 1;
+    let totalItems;
+    Product.find().count().then(numProducts=>{
+        totalItems=numProducts;
+        return Product.find()
+        .skip((page-1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE)
+    })
+    .then(products => {
+        res.render('shop/product-list', {
+            prods: products,
+            pageTitle: 'Products',
+            path: '/products',
+            currentPage: page,
+            hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+            hasPreviousPage: page > 1,
+            nextPage: page + 1,
+            previousPage : page - 1,
+            lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
+            isAuthenticated: req.session.isLoggedIn
+        });
+    })
         .catch(err => {
             const error  = new Error(err);
             error.httpStatusCode=500;
@@ -40,15 +53,28 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-    Product.find()
-        .then(products => {
-            res.render('shop/index', {
-                prods: products,
-                pageTitle: 'Shop',
-                path: '/',
-                isAuthenticated: req.session.isLoggedIn
-            });
-        })
+    const page = +req.query.page || 1;
+    let totalItems;
+    Product.find().count().then(numProducts=>{
+        totalItems=numProducts;
+        return Product.find()
+        .skip((page-1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE)
+    })
+    .then(products => {
+        res.render('shop/index', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/',
+            currentPage: page,
+            hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+            hasPreviousPage: page > 1,
+            nextPage: page + 1,
+            previousPage : page - 1,
+            lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
+            isAuthenticated: req.session.isLoggedIn
+        });
+    })
         .catch(err => {
             const error  = new Error(err);
             error.httpStatusCode=500;
