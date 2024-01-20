e11bc8ec5b1138d9d5ba5261399330ecc2c0f30b
r4ghuveer <raghuveerofficial08@gmail.com>
2024-01-20 23:55:50 +0530

    -

--- app.js
+++ app.js
@@ -101,8 +101,9 @@ app.use((error,req,res,next)=>{
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
+      
     app.listen(3000);
-      console.log("connected to database");
+      console.log("connected to database!!");
   })
   .catch(err => {
     console.log(err);
